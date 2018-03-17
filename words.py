b3c183f78daa11d4de1ca8be3d6874c573abc526
Brian Sukowicz <briansukowicz@gmail.com>
2018-03-16 23:37:29 -0500

    Add files via upload

--- words.py
+++ words.py
@@ -1,45 +1,74 @@
 import codecs
+import sys
 from tkinter.filedialog import askopenfilename
 
-def word_count(str): #Pass String. Return words and num of appearances
+def getFilePath(): #open dialog and get filepath
+    filename = askopenfilename()
+    if (not filename):
+        sys.exit()
+    file = codecs.open(filename,'r', encoding='utf-8')
+    return file
+
+def fileToListOfLines(myFile): #converts text in file into list of lines
+    listOfLines = myFile.readlines() #turns file into list of strings
+    myFile.close()
+    return listOfLines
+
+def listToString(myList): #converts list to a big string
+    stringFromList = ""
+    for x in range(len(myList)):
+        myList[x] = myList[x].replace("\n", " ") #removes new line
+        myList[x] = myList[x].replace("\t", "") #removes tab
+        stringFromList = stringFromList + myList[x] 
+    return stringFromList
+
+def stringToListOfWords(myStr): #converts a string into list of words
+    words = myStr.split()
+    return words
+
+def editListOfWords(words):
+    for x in range(len(words)):
+        words[x] = words[x].capitalize()
+        words[x] = words[x].replace('.', "")
+        words[x] = words[x].replace(",", "")
+        words[x] = words[x].replace("?", "")
+        words[x] = words[x].replace("!", "")
+        words[x] = words[x].replace(";", "")
+        words[x] = words[x].replace(":", "")
+        words[x] = words[x].replace("(", "")
+        words[x] = words[x].replace(")", "")
+        words[x] = words[x].replace("{", "")
+        words[x] = words[x].replace("}", "")
+        words[x] = words[x].replace("/", "")
+        words[x] = words[x].replace("[", "")
+        words[x] = words[x].replace("]", "")
+        words[x] = words[x].replace("'", "")
+        words[x] = words[x].replace("'s", "")
+        words[x] = words[x].replace('"', "")
+    return words
+
+def getWordCount(words):
     counts = dict()
-    words = str.split()
     for word in words:
-        word = word.capitalize()
         if word in counts:
             counts[word] += 1
         else:
             counts[word] = 1
     return counts
 
-def fileToString():#opens files and converts it into a string. Prints it
-    filename = askopenfilename()
-    file = codecs.open(filename,'r', encoding='utf-8')
-    listOfLines = file.readlines() #turns file into list of strings
-    sizeOfList = len(listOfLines) 
-    stringFromList = ""
-    for x in range(sizeOfList): #converts list into one big string
-        listOfLines[x] = listOfLines[x].replace("\n", " ")
-        listOfLines[x] = listOfLines[x].replace("\t", "")
-        listOfLines[x] = listOfLines[x].replace(".", "")
-        listOfLines[x] = listOfLines[x].replace("'s", "")
-        listOfLines[x] = listOfLines[x].replace(",", "")
-        listOfLines[x] = listOfLines[x].replace(":", "")
-        listOfLines[x] = listOfLines[x].replace("?", "")
-        listOfLines[x] = listOfLines[x].replace("!", "")
-        stringFromList = stringFromList + listOfLines[x] #One big string
-    print(narrowedDownWords(word_count(stringFromList)))
-
-def narrowedDownWords(counts):
+def printWordCounts(counts):
     for w in sorted(counts, key=counts.get, reverse=True): #sorts dict
             if(counts[w] > 0):
-                print (w, counts[w])
+                print (w, ":", counts[w])
     
+ 
     
-        
 def main():
-    fileToString()
+    printWordCounts(getWordCount(editListOfWords(stringToListOfWords(listToString(fileToListOfLines(getFilePath()))))))
     
 
 if __name__ == "__main__":
     main()
+    
+    
+    
